efb897beb4b3892c747c60a31e068dc3b5b5a29f
mpessa237 <hervempessa7@gmail.com>
2026-06-18 15:11:21 +0100

    first commit

---
+++ src/test/java/com/example/JobKmer/JobKmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JobKmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobKmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
